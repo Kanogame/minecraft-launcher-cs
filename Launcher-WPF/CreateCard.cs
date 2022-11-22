c4f72beace1981852dd6d3933a90c51137c12bc8
kanogame <wasdqert1222@gmail.com>
2022-11-22 16:31:09 +0700

    CreateCard moved, server data display working

---
+++ Launcher-WPF/CreateCard.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Launcher_WPF
+{
+    internal class CreateCard
+    {
+        private int height = 160;
+
+        private FontFamily RobotoBold = new FontFamily(new Uri("file://Fonts/Roboto-Bold.ttf"), "RobotoBold");
+        private FontFamily RobotoRegular = new FontFamily(new Uri("file://Fonts/Roboto-Regular.ttf"), "RobotoRegular");
+
+        private StackPanel ServerList;
+        private ColumnDefinition ServersCol;
+
+        public CreateCard(StackPanel ServerList, ColumnDefinition ServersCol)
+        {
+            this.ServersCol = ServersCol;
+            this.ServerList = ServerList;
+        }
+
+        public void CreateServerCard(LinearGradientBrush GradientBrush, string Name, string ip, string Version, string Desc)
+        {
+            Border bord = new Border()
+            {
+                Padding = new Thickness(12, 12, 12, 12),
+                Margin = new Thickness(0, 0, 0, 20),
+                Width = ServersCol.Width.Value - 20,
+                Height = height,
+                Background = GradientBrush,
+                CornerRadius = new CornerRadius(16, 16, 16, 16),
+            };
+            StackPanel rec = new StackPanel();
+
+            var ServerName = CreateTexBlock(Name, 30, RobotoBold);
+            var Players = CreateTexBlock(ip, 18, RobotoBold);
+            var Ver = CreateTexBlock(Version, 18, RobotoBold);
+            var Descr = CreateTexBlock(Desc, 15, RobotoRegular);
+
+            bord.Child = rec;
+            rec.Children.Add(ServerName);
+            rec.Children.Add(Players);
+            rec.Children.Add(Ver);
+            rec.Children.Add(Descr);
+
+            ServerList.Children.Add(bord);
+            ServerList.HorizontalAlignment = HorizontalAlignment.Center;
+            ServerList.CanVerticallyScroll = true;
+        }
+
+        private TextBlock CreateTexBlock(string text, int fontsize, FontFamily font)
+        {
+            TextBlock value = new TextBlock()
+            {
+                Text = text,
+                FontSize = fontsize,
+                Foreground = Brushes.White,
+                FontFamily = font,
+            };
+            return value;
+        }
+
+        public LinearGradientBrush GradientCreator(Color stop1, Color stop2)
+        {
+            LinearGradientBrush GradientBrush = new LinearGradientBrush();
+            GradientBrush.StartPoint = new Point(0, 0);
+            GradientBrush.EndPoint = new Point(1, 2);
+            GradientBrush.GradientStops.Add(new GradientStop(stop1, 0.0));
+            GradientBrush.GradientStops.Add(new GradientStop(stop2, 1));
+            return GradientBrush;
+        }
+    }
+}
--- Launcher-WPF/MainWindow.xaml.cs
+++ Launcher-WPF/MainWindow.xaml.cs
@@ -21,88 +21,37 @@ namespace Launcher_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int height = 160;
 
-        private FontFamily RobotoBold = new FontFamily(new Uri("file://Fonts/Roboto-Bold.ttf"), "RobotoBold");
-        private FontFamily RobotoRegular = new FontFamily(new Uri("file://Fonts/Roboto-Regular.ttf"), "RobotoRegular");
-
-        goConn GoConn = new goConn("127.0.0.1", 8081);
+        goConn GoConn;
+        CreateCard createCard;
 
         public MainWindow()
         {
             InitializeComponent();
+            GoConn = new goConn("127.0.0.1", 8081);
+            createCard = new CreateCard(ServerList, ServersCol);
             LoadServerList();
         }
 
         private void LoadServerList()
         {
-            var test = GoConn.GetServers();
-            for (int i = 0; i < test.GetLength(0); i++)
-            {
-                for (int j = 0; j < test.GetLength(1); j++)
+            try
+            { 
+                var ServersList = GoConn.GetServers();
+
+                for (int i = 0; i < ServersList.GetLength(0); i++)
                 {
-                    MessageBox.Show(test[i, j] + " ");
+                    createCard.CreateServerCard(createCard.GradientCreator(Color.FromRgb(91, 195, 255), Color.FromRgb(58, 160, 255)),
+                        ServersList[i, 0],
+                        ServersList[i, 1],
+                        ServersList[i, 2],
+                        ServersList[i, 3]);
                 }
             }
-            LinearGradientBrush GradientBrush = new LinearGradientBrush();
-            GradientBrush.StartPoint = new Point(0, 0);
-            GradientBrush.EndPoint = new Point(1, 2);
-
-            for (int i = 0; i < 3; i++)
+            catch (Exception)
             {
-                CreateServerCard(GradientCreator(GradientBrush, Color.FromRgb(91, 195, 255), Color.FromRgb(58, 160, 255)), "WastCraft", 4, 10, "1.19.2", "Ванильный приватный майнкрафт сервер");
+                createCard.CreateServerCard(new LinearGradientBrush(Color.FromRgb(39, 37, 55), Color.FromRgb(39, 37, 55), 0),"Error Occured", "", "", "Server is currently not anvalible");
             }
-        }
-
-        private void CreateServerCard(LinearGradientBrush GradientBrush, string Name, int CurrentPlayers, int MaxPlayer, string Version, string Desc)
-        {
-            Border bord = new Border()
-            {
-                Padding = new Thickness(12, 12, 12, 12),
-                Margin = new Thickness(0, 0, 0, 20),
-                Width = ServersCol.Width.Value - 20,
-                Height = height,
-                Background = GradientBrush,
-                CornerRadius = new CornerRadius(16, 16, 16, 16),
-            };
-            StackPanel rec = new StackPanel();
-
-            var ServerName = CreateTexBlock(Name, 30, RobotoBold);
-            var Players = CreateTexBlock($"{CurrentPlayers} / {MaxPlayer}", 18, RobotoBold);
-            var Ver = CreateTexBlock(Version, 18, RobotoBold);
-
-            bord.Child = rec;
-            rec.Children.Add(ServerName);
-            rec.Children.Add(Players);
-            rec.Children.Add(Ver);
-
-            ServerList.Children.Add(bord);
-            ServerList.HorizontalAlignment = HorizontalAlignment.Center;
-            ServerList.CanVerticallyScroll = true;
-        }
-
-        private TextBlock CreateTexBlock(string text, int fontsize, FontFamily font)
-        {
-            TextBlock value = new TextBlock()
-            {
-                Text = text,
-                FontSize = fontsize,
-                Foreground = Brushes.White,
-                FontFamily = font,
-            };
-            return value;
-        }
-
-        private LinearGradientBrush GradientCreator(LinearGradientBrush GradientBrush, Color stop1, Color stop2)
-        {
-            GradientBrush.GradientStops.Add(new GradientStop(stop1, 0.0));
-            GradientBrush.GradientStops.Add(new GradientStop(stop2, 1));
-            return GradientBrush;
-        }
-
-        private void GetServerData()
-        {
-            throw new NotImplementedException();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
